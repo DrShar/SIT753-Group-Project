e608839749f74bddaec8aaa673d618985ea2db7f
Sharath Vijayananda <dr.sharathvijayananda@gmail.com>
2024-05-12 15:07:27 +1000

    Content: Add Unit Content and Fee link in Custom Current Students Button

---
+++ script.js
@@ -0,0 +1,8 @@
+//
+// This is the Javascript file I have included to enable various functions of my code such as Button Toggles, Link Redirections, etc. ~ Sharath Vijayananda 
+// 
+
+// The below function is for the 'Current Students' button to redirect to the relevant Enrolment and Fees url I have included in the html file 
+function redirectTo(url) {
+ window.open(url, '_blank')}; // used _blank so that the Enrolment and Fees url opens in a new tab, while leaving the main tab of the SIT753 Unit Site as is 
+
